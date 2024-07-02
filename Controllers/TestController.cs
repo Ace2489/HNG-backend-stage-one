4f1821f1d180e18d577eb39de8b6145173367ad0
Covenant Okoroafor <Okoroaforcovenant99@gmail.com>
2024-07-02 14:50:36 +0100

    Added functionality to get IP address of client

--- Controllers/TestController.cs
+++ Controllers/TestController.cs
@@ -1,3 +1,4 @@
+using System.Net;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HNG_backend_stage_one.Controllers;
@@ -9,8 +10,27 @@ public class HelloController : ControllerBase
     [HttpGet]
     public IActionResult Hello([FromQuery] string visitor)
     {
-        string queryString = visitor;
 
-        return Ok(new { Message = string.Format("Hello, {0}", visitor)});
+        string ipAddress = GetIPAddress();
+        return Ok(new
+        {
+            client_ip = ipAddress,
+            location = "Not yet implemented",
+            greeting = $"Hello, {visitor}"
+        });
+    }
+
+    private string GetIPAddress()
+    {
+        IPAddress? iPAddress = HttpContext.Connection.RemoteIpAddress;
+        if (iPAddress == null)
+        {
+            return "unknown";
+        }
+        if (Request.Headers.TryGetValue("X-Forwarded-For", out Microsoft.Extensions.Primitives.StringValues value))
+        {
+            return value!;
+        }
+        return iPAddress.ToString();
     }
 }
